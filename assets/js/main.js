86fd1f35fbd19cae523014ef36d97d198102c242
joshua-tree-ctrl <55064706+joshua-tree-ctrl@users.noreply.github.com>
2020-01-16 22:44:34 +0000

    API tests

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,12 +13,13 @@ $.ajax({
 $.ajax({
     type: "GET",
     url:"https://api.openweathermap.org/data/2.5/weather?q=" + location.city + "&units=metric&appid=8ec99075200cb619dd999c05b1906c24",
+    //url:"https://api.openweathermap.org/data/2.5/weather?q=london&units=metric&appid=8ec99075200cb619dd999c05b1906c24",
     success: function(weather) {
     console.log(weather);
     console.log(weather.weather[0].icon);
         weatherData=weather; //could store response in global variable, but as this is asynchronous (happens regardless in background), the variable wont be filled/usable until call is complete and therefore may create page load issues. Can test later if required. 
-    $('h1').html(weather.name + " " + weather.main.temp + " " + weather.main.temp + " " + weather.main.temp_max + " " + weather.main.temp_min  );
-    $('.weather__icon').append("<img src=\"http://openweathermap.org/img/wn/"  + weather.weather[0].icon + "@2x.png\" alt=\"image weather\">");
+    $('h1').html(weather.name + " " + weather.main.temp + " " + weather.main.temp + " " + weather.main.temp_max + " " + weather.main.temp_min + " " + weather.weather[0].main + " " + weather.weather[0].description );
+    $('.weather__icon').append("<img src=\"http://openweathermap.org/img/wn/"  + weather.weather[0].icon + "@2x.png\" alt=\"image weather\" class=\"weather__icon--img\">");
      
    
     },
