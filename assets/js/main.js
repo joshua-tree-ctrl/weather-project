c0eaa3e9435df60887d10969f4001b59f35a666b
joshua-tree-ctrl <55064706+joshua-tree-ctrl@users.noreply.github.com>
2020-01-17 23:14:35 +0000

    API Integration
    Integrating new API, adding jQuery selectors

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,14 +6,57 @@ $.ajax({
     jsonpCallback: "callback",
     dataType: "jsonp",
     success: function(location) {
-    console.log(location);
+   // console.log(location);
 
      //Open Weather Map API AJAX Call inside geolocation API, as the geolocation call is in jsonp data type which does not support synchronous operation, so I cannot simply pass initial ajax call as false. Could do callback function with ajax to obtain data, and then build the html from that callback function data? alternative could be handlebars?
 $.ajax({
     type: "GET",
-    url:"https://api.weatherbit.io/v2.0/forecast/daily?city=" + location.city + "," + location.country_code + "&days=5&key=959dca19c5aa40b084f4991fa3a58145",
+    url:"https://api.weatherbit.io/v2.0/forecast/daily?city=" + location.city + "," + location.country_code + "&days=3&key=959dca19c5aa40b084f4991fa3a58145",
     success: function(weather) {
     console.log(weather);
+
+    loggedWeatherData = () => {
+
+     /* //SETTING DATE & TIME IN SHORTHAND
+      let date = new Date();
+      var options = {  month: 'numeric', day: 'numeric' };
+      let displayDate = date.toLocaleDateString("en-US", options); */ 
+      
+      let weatherObject = {};
+      weatherObject['city'] = weather.city_name;
+      weatherObject['code'] = weather.country_code;
+      weatherObject['data'] = weather.data;
+      weatherObject['time'] = weather.timezone;
+     
+      // console.log(weatherObject);
+      $('.weather__location').append("Area " + weatherObject.city);
+      $('.weather__location').append(" Country " + weatherObject.code);
+     //DATE LOOP 
+     //ICON LOOP
+     //TEMP LOOP 
+     //HIGH TEMP LOOP
+     //LOW TEMP LOOP
+     //https://www.weatherbit.io/static/img/icons/r01d.png EXAMPLE IMG 
+      
+
+      for (let i = 0; i < weatherObject.data.length; i++) {
+      $('.weather__date').append('<div class="weather__date--' + i + '"> Dates:    ' + weatherObject.data[i].valid_date + '       </div>');
+      $('.weather__icon').append("<img src=\"https://www.weatherbit.io/static/img/icons/"  + weatherObject.data[i].weather.icon + ".png\" alt=\"weather icon\" class=\"weather__icon--\">");  
+      $('.weather__desc').append('<div class="weather__desc--' + i + '">  Descriptions:  ' + weatherObject.data[i].weather.description + '</div>');
+      $('.weather__temp').append('<div class="weather__temp--' + i + '">  Temps:  ' + weatherObject.data[i].temp + '</div>');  
+      $('.weather__temp').append('<div class="weather__temp__high--' + i + ' ">  Highs Of :   ' + weatherObject.data[i].max_temp + '</div>');  
+      $('.weather__temp').append('<div class="weather__temp__high--' + i + ' "> Lows Of   : ' + weatherObject.data[i].min_temp + '</div>');  
+        } 
+
+
+      return weatherObject; 
+    };
+    loggedWeatherData();
+    //console.log(loggedWeatherData());
+
+      
+
+
      }, 
     beforeSend: function() {
     console.log("loading");
