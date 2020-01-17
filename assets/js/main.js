46fa414a22f21ec78db06bd757c3779abd23a7e4
Hayden Ismet <haydenismet@gmail.com>
2020-01-17 18:51:37 +0000

    API Update
    Amended API from openweathermap to Weatherbit for easier daily forecasting - and it's still free!

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,3 @@
-console.log("Hello World");
-var weatherData;
 
 // Initial AJAX Call to Geolocate user, on success of obtaining data, chain second AJAX call and add the location.city to the AJAX URL. 
 $.ajax({
@@ -9,20 +7,14 @@ $.ajax({
     dataType: "jsonp",
     success: function(location) {
     console.log(location);
+
      //Open Weather Map API AJAX Call inside geolocation API, as the geolocation call is in jsonp data type which does not support synchronous operation, so I cannot simply pass initial ajax call as false. Could do callback function with ajax to obtain data, and then build the html from that callback function data? alternative could be handlebars?
 $.ajax({
     type: "GET",
-    url:"https://api.openweathermap.org/data/2.5/weather?q=" + location.city + "," + location.country_code + "&units=metric&appid=8ec99075200cb619dd999c05b1906c24",
-   // url:"https://api.openweathermap.org/data/2.5/weather?q=london&units=metric&appid=8ec99075200cb619dd999c05b1906c24",
+    url:"https://api.weatherbit.io/v2.0/forecast/daily?city=" + location.city + "," + location.country_code + "&days=5&key=959dca19c5aa40b084f4991fa3a58145",
     success: function(weather) {
     console.log(weather);
-    console.log(weather.weather[0].icon);
-        weatherData=weather; //could store response in global variable, but as this is asynchronous (happens regardless in background), the variable wont be filled/usable until call is complete and therefore may create page load issues. Can test later if required. 
-    $('h1').html(weather.name + " " + weather.main.temp + " " + weather.main.temp + " " + weather.main.temp_max + " " + weather.main.temp_min + " " + weather.weather[0].main + " " + weather.weather[0].description );
-    $('.weather__icon').append("<img src=\"http://openweathermap.org/img/wn/"  + weather.weather[0].icon + "@2x.png\" alt=\"image weather\" class=\"weather__icon--img\">");
-     
-   
-    },
+     }, 
     beforeSend: function() {
     console.log("loading");
     },
@@ -39,3 +31,36 @@ $.ajax({
         }
   });
   
+
+
+
+
+
+  /*
+
+      //url:"https://api.openweathermap.org/data/2.5/weather?q=" + location.city + "," + location.country_code + "&units=metric&appid=8ec99075200cb619dd999c05b1906c24",
+    //url:"https://api.openweathermap.org/data/2.5/forecast?q=" + location.city + "," + location.country_code + "&units=metric&appid=8ec99075200cb619dd999c05b1906c24",
+
+  // url:"https://api.openweathermap.org/data/2.5/weather?q=london&units=metric&appid=8ec99075200cb619dd999c05b1906c24",
+
+    //console.log(weather.weather[0].icon);
+        
+    //weatherData=weather; //could store response in global variable, but as this is asynchronous (happens regardless in background), the variable wont be filled/usable until call is complete and therefore may create page load issues. Can test later if required. 
+
+    // $('h1').html(weather.name + " " + weather.sys.country + " " + weather.main.temp + " " + weather.main.temp_max + " " + weather.main.temp_min + " " + weather.weather[0].main + " " + weather.weather[0].description );
+
+    //$('.weather__icon').append("<img src=\"http://openweathermap.org/img/wn/"  + weather.weather[0].icon + "@2x.png\" alt=\"image weather\" class=\"weather__icon--img\">");
+
+    
+      loggedWeatherData = () => {
+        let weatherArray = [];
+        for (let i = 0; i < weather.list.length; i++) {
+        weatherArray.push(weather.list[i]);
+        
+        console.log(weatherArray[i].main.temp);
+        }
+        return weatherArray;
+      }
+      loggedWeatherData();
+      //console.log(loggedWeatherData());
+   */
